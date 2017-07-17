feb60565305caf44f6f01328db232ead2410f6cb
solics <s.huayhua.ma@gmail.com>
2017-07-17 11:57:44 -0500

    subiendo funcion imc

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function imc(){
-	var peso = parseInt(prompt("Ingrese su peso","En kilogramos..."));
-	var estatura = parseFloat(prompt("Ingrese su estatura","1.70..."));
-	var imc = parseInt(peso/(estatura*estatura));
-	alert("Su IMC es "+imc+"\n\nPeso inferior al normal----Menos de 18.5\nNormal----18.5 – 24.9\nPeso superior al normal----25.0 – 29.9\nObesidad----Más de 30.0");
+	var peso = parseFloat(prompt("Ingrese su peso","58.5"));
+	var estatura = parseFloat(prompt("Ingrese su estatura","1.70"));
+	var imc = peso/(estatura*estatura);
+	alert("Su IMC es "+imc.toFixed(1)+"\n\nPeso inferior al normal----Menos de 18.5\nNormal----18.5 – 24.9\nPeso superior al normal----25.0 – 29.9\nObesidad----Más de 30.0");
 }
 imc();
